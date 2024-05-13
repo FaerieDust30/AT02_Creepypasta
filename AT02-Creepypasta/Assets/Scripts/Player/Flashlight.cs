b1f49842995cb6ac6f5f98257a74bbc0404d7be4
FaerieDust30 <160442076+FaerieDust30@users.noreply.github.com>
2024-05-13 22:46:05 +0800

    Flashlight scripting
    Worked on the flashlight script and got it to degrade down nicely.
    Then worked on getting it to "recharge" when F is pressed which sort of works but it doesn't follow chargeSpeed like I want it to.
    The flashlight degrading method also has a problem where using the current intensity breaks it.

--- AT02-Creepypasta/Assets/Scripts/Player/Flashlight.cs
+++ AT02-Creepypasta/Assets/Scripts/Player/Flashlight.cs
@@ -8,24 +8,54 @@ public class Flashlight : MonoBehaviour
 {
     public float maxIntensity = 5f;
     public float minIntensity = 0f;
-    public float fadeSpeed = 0.5f;
+    public float fadeSpeed;
+    public float fadeTime;
+    public float chargeSpeed;
+    public float chargeTime;
 
-    private Light light;
+    private Light fLight;
 
-    private void Awake()
-    {
-
-    }
     // Start is called before the first frame update
     void Start()
     {
-        light = GetComponent<Light>();
+        fLight = GetComponent<Light>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        light.intensity = Mathf.Lerp(minIntensity, maxIntensity, Time.time/10);
+        flashlightFade();
+        flashlightCrank();
     }
 
+    private void flashlightCrank()
+    {
+        if (Input.GetButton("Flashlight") == true)
+        {
+            chargeTime = Time.deltaTime / chargeSpeed;
+            fLight.intensity = Mathf.Lerp(fLight.intensity, maxIntensity, chargeTime);
+            fadeTime = 0f;
+            if(chargeTime > chargeSpeed)
+            {
+                chargeTime = chargeSpeed;
+            }
+        }
+    }
+
+    private void flashlightFade()
+    {
+        if (Input.GetButton("Flashlight") == false)
+        {
+            chargeTime = 0f;
+            if (fadeTime < fadeSpeed)
+            {
+                fLight.intensity = Mathf.Lerp(fLight.intensity, minIntensity, fadeTime / fadeSpeed);
+                fadeTime += Time.deltaTime;
+                if (fadeTime > fadeSpeed)
+                {
+                    fadeTime = fadeSpeed;
+                }
+            }
+        }
+    }
 }
